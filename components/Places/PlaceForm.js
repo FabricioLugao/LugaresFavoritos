9fe468082c3b007270f21c33abd88a10ac88ed2b
DESKTOP-GO7PEOM\DELL <fabriciolugao@gmail.com>
2024-06-20 18:22:12 -0300

    Comit inicial

---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function PlaceForm() {
+  return (
+    <View>
+      <Text>O formulário de lugar</Text>
+    </View>
+  );
+}
+
+export default PlaceForm;
